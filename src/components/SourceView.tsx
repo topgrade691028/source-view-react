52ff06e5661f29cfba358a86295d7c3af9410d86
Eros720 <ninessander720@gmail.com>
2020-01-16 16:51:56 +0800

    feat: render syntax

--- src/components/SourceView.tsx
+++ src/components/SourceView.tsx
@@ -1,22 +1,29 @@
 import {useMemo, FC, ReactNode} from 'react';
-import {LineOfSyntax, SyntaxElement} from 'source-tokenizer';
+import {LineOfSyntax, SyntaxElement, TreeNode} from 'source-tokenizer';
 
-export type RenderSyntaxElement = (node: SyntaxElement, defaultRender: RenderSyntaxElement, i: number) => ReactNode;
+export type RenderSyntaxTree = (root: TreeNode, defaultRender: RenderSyntaxElement, i: number) => ReactNode;
 
-export interface SourceViewProps {
-    source?: string;
-    syntax?: LineOfSyntax[];
-    renderSyntaxTree?: RenderSyntaxElement;
+export type RenderSyntaxElement = (element: SyntaxElement, i: number) => ReactNode;
+
+interface TextViewProps {
+    source: string;
 }
 
-const renderSyntaxElement = (element: SyntaxElement, i: number): ReactNode => {
+interface SyntaxViewProps {
+    syntax: LineOfSyntax[];
+    renderSyntaxTree?: RenderSyntaxTree;
+}
+
+export type SourceViewProps = TextViewProps & SyntaxViewProps;
+
+const renderGenericElement = (element: SyntaxElement, i: number): ReactNode => {
     if (typeof element === 'string') {
         return element;
     }
 
     return (
         <span key={i} className={element.properties?.className?.join(' ')}>
-            {element.children.map(renderSyntaxElement)}
+            {element.children.map(renderGenericElement)}
         </span>
     );
 };
@@ -25,30 +32,48 @@ const renderGutter = (line: number): ReactNode => <td className="source-gutter" 
 
 const renderSourceContent = (source: string): ReactNode => <td className="source-code">{source}</td>;
 
-const renderSyntaxContent = (syntax: LineOfSyntax): ReactNode => syntax.map(renderSyntaxElement);
+const renderSyntaxContent = (syntax: LineOfSyntax, {renderSyntaxTree}: SourceViewProps): ReactNode => {
+    const render = (element: SyntaxElement, i: number) => {
+        if (typeof element === 'string') {
+            return element;
+        }
 
-const renderLine = (content: string | LineOfSyntax, i: number): ReactNode => (
+        if (renderSyntaxTree) {
+            return renderSyntaxTree(element, renderGenericElement, i);
+        }
+
+        return renderGenericElement(element, i);
+    };
+
+    return <td className="source-code">{syntax.map(render)}</td>;
+};
+
+const renderSourceLine = (content: string, i: number): ReactNode => (
     <tr key={i} className="source-line">
         {renderGutter(i + 1)}
-        {typeof content === 'string' ? renderSourceContent(content) : renderSyntaxContent(content)}
+        {renderSourceContent(content)}
     </tr>
 );
 
-export const SourceView: FC<SourceViewProps> = ({syntax, source}) => {
+const renderSyntaxLine = (content: LineOfSyntax, i: number, props: SourceViewProps): ReactNode => (
+    <tr key={i} className="source-line">
+        {renderGutter(i + 1)}
+        {renderSyntaxContent(content, props)}
+    </tr>
+);
+
+export const SourceView: FC<SourceViewProps> = props => {
+    const {source, syntax} = props;
     const lines = useMemo(() => source?.split('\n') ?? [], [source]);
-
-    if (syntax) {
-        return null;
-    }
-
-    if (typeof source !== 'string') {
-        throw new Error('react-source-view requires either source or syntax prop');
-    }
 
     return (
         <table className="source">
             <tbody>
-                {lines.map(renderLine)}
+                {
+                    syntax
+                        ? syntax.map((item, i) => renderSyntaxLine(item, i, props))
+                        : lines.map(renderSourceLine)
+                }
             </tbody>
         </table>
     );
